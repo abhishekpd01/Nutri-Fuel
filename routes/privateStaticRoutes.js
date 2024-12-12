599a9c143a8749445f2f37bf1ff6159e78825df4
Abhishek Prasad <prasadabhishek550@gmail.com>
2024-12-12 23:05:49 +0530

    minor change

--- routes/privateStaticRoutes.js
+++ routes/privateStaticRoutes.js
@@ -1,16 +1,6 @@
 const express = require("express");
-// const path = require("path");
 const router = express.Router();
 exports.router = router;
-
-// GET requests
-// router.get('/signup', (req, res) => {
-//     res.render('signup');
-// })
-
-// router.get('/login', (req, res) => {
-//     res.render('login');
-// })
 
 router.get("/userInfo", (req, res) => {
     res.render('userInfo');
@@ -19,22 +9,6 @@ router.get("/userInfo", (req, res) => {
 router.get("/result", (req, res) => {
     res.render('result');
 })
-
-// router.get("/", function(req, res) {
-//     res.render("index");
-// });
-
-// router.get("/About", function(req, res) {
-//     res.sendFile(path.join(__dirname + "/../views/about.html"));
-// });
-
-// router.get("/Contact", function(req, res) {
-//     res.sendFile(path.join(__dirname + "/../views/contact.html"));
-// });
-
-// router.get("/Session", function(req, res) {
-//     res.sendFile(path.join(__dirname + "/../views/session.html"));
-// });
 
 router.get("/TrackWorkout", function(req, res) {
     res.render("trackWorkout", { submitted: false });
