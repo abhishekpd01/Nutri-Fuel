418199ace117468086fdcc6592f6e4828dbe816a
Abhishek Prasad <prasadabhishek550@gmail.com>
2025-02-03 23:35:11 +0530

    updated openai dependency to latest version in order to use gemini 2.0 flash from openrouter.ai and updated the openai config

--- config/openaiConfig.js
+++ config/openaiConfig.js
@@ -1,8 +1,15 @@
-const { Configuration, OpenAIApi } = require('openai');
+const OpenAI = require('openai');
 
 // Initialize OpenAI API
-const openai = new OpenAIApi(new Configuration( {
+
+const openai = new OpenAI({
+    basURL: 'https://openrouter.ai/api/v1',
     apiKey: process.env.API_KEY
-}));
+});
+
+// This is older configuration openai version 3.3.0
+/* const openai = new OpenAIApi(new Configuration( {
+    apiKey: process.env.API_KEY
+})); */
 
 module.exports = openai;
