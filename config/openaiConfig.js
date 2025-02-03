ef0b55bc83801d89de225310ef59944db8693cca
Abhishek Prasad <prasadabhishek550@gmail.com>
2025-02-04 00:43:53 +0530

    feat: integrate Google Gemini AI and update OpenAI configuration

--- config/openaiConfig.js
+++ config/openaiConfig.js
@@ -1,7 +1,11 @@
 const OpenAI = require('openai');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Initialize Gemini API
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 // Initialize OpenAI API
-
 const openai = new OpenAI({
     basURL: 'https://openrouter.ai/api/v1',
     apiKey: process.env.API_KEY
@@ -12,4 +16,7 @@ const openai = new OpenAI({
     apiKey: process.env.API_KEY
 })); */
 
-module.exports = openai;
+module.exports = {
+    openai,
+    model
+};
